642d3d37bcb0d026cac8a70c17b65c11fc0afb80
dddddddaaa87 <dddddddaaa87@mail.ru>
2023-01-16 19:32:15 +0300

    Добавил CheckController

---
+++ Dudak/Controllers/CheckController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System;
+
+namespace Durak.Controllers
+{
+    [ApiController]
+    [Route("/api/v1/check")]
+    public class CheckController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<IActionResult> CheckByIDAsync(int id)
+        {
+            try
+            {
+                return Ok(new { result = id });
+            }
+            catch (Exception ex)
+            {
+                //Request.Body.Position = 0;
+                //var rawRequestBody = await new StreamReader(Request.Body).ReadToEndAsync();
+                //await _requestService.SetLogAsync(Request.Path.Value, rawRequestBody, ex.Message);
+                return BadRequest(new { status = 400, error = ex.Message });
+            }
+        }
+
+        [HttpGet("string")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<IActionResult> CheckByStringAsync()
+        {
+            try
+            {
+                return Ok(new { result = "Ok" });
+            }
+            catch (Exception ex)
+            {
+                //Request.Body.Position = 0;
+                //var rawRequestBody = await new StreamReader(Request.Body).ReadToEndAsync();
+                //await _requestService.SetLogAsync(Request.Path.Value, rawRequestBody, ex.Message);
+                return BadRequest(new { status = 400, error = ex.Message });
+            }
+        }
+    }
+}
